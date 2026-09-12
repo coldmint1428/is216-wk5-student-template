55b3a70341ab41f1cf4bb0909cb198067fd423f4
coldmint1428 <chwkm7656@gmail.com>
2026-09-13 01:32:45 +0800

    Update Ex4.vue

--- src/components/Ex4.vue
+++ src/components/Ex4.vue
@@ -4,6 +4,16 @@ const items = ref(['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter'])
 
 // TODO: Add Code Here
 
+const add = ref('')
+
+function addItem(){
+    items.value.push(add.value)
+    add.value = ''
+}
+
+function deleteItem(index){
+    items.value.splice(index,1)
+}
 
 </script>
 
@@ -11,6 +21,16 @@ const items = ref(['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter'])
 
     <h2>Shopping Cart</h2>
     <!-- TODO: Add Code Here -->
+    <ul>
+        <li v-for="(item, index) in items">
+            {{ item }}
+            <button type="button" v-on:click="deleteItem(index)">Delete</button>
+        </li>
+    </ul>
 
+    <input type="text" v-model="add">
+    <button type="button" v-on:click="addItem">Add!</button>
+
+    
    
 </template>
