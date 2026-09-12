6e0f467902a5ef4e6e381939dc7d354a0dcdebd5
coldmint1428 <chwkm7656@gmail.com>
2026-09-13 01:03:35 +0800

    Update Ex2.vue

--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -45,13 +45,8 @@ function reset() {
 
         <div style="margin-top: 14px;">
             <!-- C. Display winner / status here -->
-            <P v-if ="scoreA === maxScore">
-                Winner {{ teamA }}
-            </P>
-
-            <P v-else-if ="scoreB === maxScore">
-                Winner {{ teamB }}
-            </P>
+            <p v-if="scoreA === maxScore">Winner: {{ teamA.toUpperCase() }}</p>
+            <p v-else-if="scoreB === maxScore">Winner: {{ teamB.toUpperCase() }}</p>
 
             <p v-else>
                 No winner yet. Keep playing!
